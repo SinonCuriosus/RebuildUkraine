8750ba1204ae5414561adc9eb72890a225e00108
SinonCuriosus <davidrmh@live.com>
2022-04-28 21:36:40 +0100

    Update dos Models;

--- rebuildukraine/slavaukraine/urls.py
+++ rebuildukraine/slavaukraine/urls.py
@@ -7,24 +7,28 @@ app_name='slavaukraine'
 urlpatterns = [
     #http://127.0.0.1:8000/slavaukraine
     path("",views.home_screen, name="home"),
+
     #http://127.0.0.1:8000/slavaukraine/register
     path("register",views.personRegistration_view, name="registerperson"),
+
     # http://127.0.0.1:8000/slavaukraine/register_enterprise
     path("register_enterprise", views.enterpriseRegistration_view, name="registerenterprise"),
+
     #http://127.0.0.1:8000/slavaukraine/login
     path("login/",views.login_view, name="login"),
+
     #http://127.0.0.1:8000/slavaukraine/logout
     path("logout/",views.logout_view, name="logout"),
-    # http://127.0.0.1:8000/slavaukraine/logout
 
-
+    #http://127.0.0.1:8000/slavaukraine/logout
     path("reserved",views.reserved, name="reserved"),
 
+    #http://127.0.0.1:8000/slavaukraine/volunteer
     path("volunteer",views.volunteer, name="volunteer"),
 
+    #http://127.0.0.1:8000/slavaukraine/enterprise
     path("enterprise",views.enterprise, name="enterprise"),
 
+    #http://127.0.0.1:8000/slavaukraine/contacts
     path("contacts",views.contacts, name="contacts"),
-
-
 ]
