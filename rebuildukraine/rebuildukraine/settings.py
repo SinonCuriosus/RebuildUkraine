6be7b638c867ee27f6c201039def202b1f3b39cb
rauliporto <rauliporto@sapo.pt>
2022-05-04 00:06:23 +0100

    2a fase email enviar e servidço de mensagens

--- rebuildukraine/rebuildukraine/settings.py
+++ rebuildukraine/rebuildukraine/settings.py
@@ -133,3 +133,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'slavaukraine/static/slavaukraine/profile_im
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sapo.pt'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'slavaukraine@sapo.pt'
+EMAIL_HOST_PASSWORD = 'slavaukraine1A'
+DEFAULT_FROM_EMAIL = 'slavaukraine@sapo.pt'
